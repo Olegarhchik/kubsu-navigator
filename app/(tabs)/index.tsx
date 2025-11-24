009ada49f9619bdd95ca3c6dc85f6aa6c217d525
Olegarhchik <atabaev.o.k.04@gmail.com>
2025-11-24 13:57:35 +0300

    Added pan gesture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,24 +4,56 @@ import TabBar from "@/components/TabBar";
 import TabBarButton from "@/components/TabBarButton";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SvgXml } from "react-native-svg";
 
 export default function IndexScreen() {
+  const { width, height } = Dimensions.get("screen");
+
   const [showMore, setShowMore] = useState<boolean>(false)
   const router = useRouter();
+  const offset = useSharedValue<{x: number, y: number }>({ x: -1000, y: -1000})
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: offset.value.x },
+      { translateY: offset.value.y }
+    ]
+  }))
+
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      let { x, y } = offset.value
+      let [newX, newY] = [x + event.changeX, y + event.changeY]
+
+      if (-2000 < newX && newX < 0)
+        x += event.changeX;
+
+      if (-2000 < newY && newY < 0)
+        y += event.changeY
+      
+      offset.value = { x, y }
+    })
 
   return (
-    <GestureHandlerRootView>
-      <View style={{ position: "absolute" }}>
-        <SvgXml
-          height={2000}
-          width={2000}
-          xml={map.data}
-        />
-      </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={pan}>
+        <Animated.View style={[{
+            position: "absolute",
+            paddingVertical: height / 2,
+            paddingHorizontal: width / 2,
+            backgroundColor: "#fff",
+          }, animatedStyle]}>
+          <SvgXml
+            height={2000}
+            width={2000}
+            xml={map.data}
+          />
+        </Animated.View>
+      </GestureDetector>
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.container}>
           <SearchBar onPress={() => router.push("/search")} placeholder="Что ищете?" />
