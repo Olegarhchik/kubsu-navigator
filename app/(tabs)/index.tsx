5c0b9de6638fa55564d7dcb1e80a08ef9d5259f7
Olegarhchik <atabaev.o.k.04@gmail.com>
2025-11-24 13:07:13 +0300

    Added map to the app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,47 @@
+import map from "@/assets/map";
 import SearchBar from "@/components/SearchBar";
 import TabBar from "@/components/TabBar";
 import TabBarButton from "@/components/TabBarButton";
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { SvgXml } from "react-native-svg";
 
 export default function IndexScreen() {
   const [showMore, setShowMore] = useState<boolean>(false)
   const router = useRouter();
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <SearchBar onPress={() => router.push("/search")} placeholder="Что ищете?" />
-        <TabBar>
-          {showMore ? 
-            <>
-              <TabBarButton onPress={() => setShowMore(prevShowMore => !prevShowMore)} label="Назад" />
-              <TabBarButton label="Отслеживаемое" href="/tracked" />
-              <TabBarButton label="Настройки" href="/settings" />
-            </> :
-            <>
-              <TabBarButton onPress={() => setShowMore(prevShowMore => !prevShowMore)} label="Дополнительно" />
-              <TabBarButton label="Маршрут" href="/route" />
-              <TabBarButton label="Расписание" href="/timetable" />
-            </>
-          }
-        </TabBar>
+    <GestureHandlerRootView>
+      <View style={{ position: "absolute" }}>
+        <SvgXml
+          height={2000}
+          width={2000}
+          xml={map.data}
+        />
       </View>
-    </SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <SearchBar onPress={() => router.push("/search")} placeholder="Что ищете?" />
+          <TabBar>
+            {showMore ? 
+              <>
+                <TabBarButton onPress={() => setShowMore(prevShowMore => !prevShowMore)} label="Назад" />
+                <TabBarButton label="Отслеживаемое" href="/tracked" />
+                <TabBarButton label="Настройки" href="/settings" />
+              </> :
+              <>
+                <TabBarButton onPress={() => setShowMore(prevShowMore => !prevShowMore)} label="Дополнительно" />
+                <TabBarButton label="Маршрут" href="/route" />
+                <TabBarButton label="Расписание" href="/timetable" />
+              </>
+            }
+          </TabBar>
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
